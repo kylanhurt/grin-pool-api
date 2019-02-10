ecf45565258d0c884e22e4ce3a2c0353fc27b3de
kylanhurt <kylan.hurt@gmail.com>
2019-02-10 01:57:05 -0800

    Get first endpoint working correclty with regular MySQL

--- src/routes/networkRoutes.js
+++ src/routes/networkRoutes.js
@@ -1,33 +1,58 @@
 const networkRouter = require('express').Router()
 const db = require('../config/database.js')
-const Block = require('../models/blocks.js')
-const Sequelize = require('sequelize')
-const Op = Sequelize.Op
+import { getConnection } from '../utils.js'
 
 // gets network data for a range of blocks
 networkRouter.get('/stats/:height,:range/:fields?', async (req, res) => {
   try {
-    const { height, range, fields } = req.params 
+    const { height, range, fields } = req.params
+    const connection = getConnection()
     const queryOpts = {}
     if (!height || !range) throw new Error('No height or range field specified')
     let max, min
     max = parseInt(height)
     const rangeNumber = parseInt(range)
-    min = max - rangeNumber    
-    queryOpts.where = {
-      height: {
-        [Op.gt]: min,
-        [Op.lte]: max
-      }
-    }
+    min = max - rangeNumber
     if (fields) {
       const fieldList = fields.split(',')
       if (fieldList.length !== 0) {
         queryOpts.attributes = fieldList
       }
     }
-    const blocks = await Block.findAll(queryOpts)
-    res.json(blocks)
+    const query = `SELECT grin_stats.difficulty, UNIX_TIMESTAMP(grin_stats.timestamp) as timestamp, grin_stats.height, gps.edge_bits, gps.gps
+      FROM grin_stats JOIN gps ON grin_stats.height = gps.grin_stats_id
+      WHERE grin_stats.height > ${connection.escape(min)} AND grin_stats.height <= ${connection.escape(max)}`
+    // console.log('query is: ', query)
+    connection.query(
+      query,
+      (error, results, field) => {
+        if (error) throw Error
+        console.log('results is: ', results)
+        let output = []
+        results.forEach((resultsRow) => {
+          const index = output.findIndex(outputRow => outputRow.height === resultsRow.height)
+          if (index === -1) {
+            output.push({ 
+              difficulty: resultsRow.difficulty,
+              timestamp: resultsRow.timestamp,
+              height: resultsRow.height,
+              gps: [
+                {
+                  edge_bits: resultsRow.edge_bits,
+                  gps: resultsRow.gps
+                }
+              ]
+            })
+          } else {
+            output[index].gps.push({
+              edge_bits: resultsRow.edge_bits,
+              gps: resultsRow.gps
+            })
+          } 
+        })
+        res.json(output)
+      }
+    )
   } catch (e) {
     console.log('Error is: ', e)
   }
